7461f660a0d2191781f3f1c00a8399dc7903119a
kmmontes <kmontes4989@gmail.com>
2022-03-23 13:59:03 -0700

    challenges part2

--- into-km-ts.js
+++ into-km-ts.js
@@ -41,3 +41,51 @@ console.log("Katherine".charAt(0))
 console.log("katherine".toUpperCase())
 
 
+// Is 34 divided by 3 greater than 67 divided by 2?
+
+console.log(34/3 > 67/2)
+
+// Does 5 evaluate to the same as "5"?
+
+console.log (5 == "5")
+
+// Does 5 strictly equal "5"?
+
+console.log (5 === "5")
+
+// Does !3 strictly equal 3?
+
+console.log(!3 === 3)
+
+// Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+
+
+
+// / let school="LEARN" -- Be sure to declare variable before inserting it in to a function--
+
+
+console.log("learn".length === 5 && "student".length === 7)
+
+// Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+
+console.log("learn".length === 5 || "student".length === 10)
+
+// Does "LEARN" contain the subset "RN"?
+
+console.log("LEARN".includes("RN"))
+
+// Does "LEARN"[0] strictly equal "l"?
+
+console.log("LEARN".charAt(0) === "l")
+
+// Modify the code from the previous question to return true.
+
+console.log("LEARN".charAt(0) === "L")
+
+
+
+
+
+
+
+
