3777ad7bcf5b6e74a606eb6ccf7a70e0b31ccb93
zmprunella <zane.prunella@gmail.com>
2022-03-23 13:45:18 -0700

    New Stuff

--- challenge-vm-zp.js
+++ challenge-vm-zp.js
@@ -3,3 +3,21 @@
 //console.log (123 - 67)
 
 //console.log(56 * 23)
+
+// console.log(45 / 5);
+
+// console.log(5 ** 7);
+
+// console.log(33 % 6);
+
+// console.log("zane and vanessa".length);
+
+// console.log("zane and vanessa".length);
+
+// var greeting = "zane and vanessa"
+
+// console.log (greeting.includes("e"));
+
+// console.log(greeting.charAt(0));
+
+// console.log(greeting.toUpperCase());
