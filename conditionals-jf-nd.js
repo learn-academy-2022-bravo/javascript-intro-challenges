dcfbaf7e72d25da7f1b797712dc3a3d3c83ab99d
JonathnSparePartsFuller <jonefuller@gmail.com>
2022-03-23 16:24:44 -0700

    Progress

--- conditionals-jf-nd.js
+++ conditionals-jf-nd.js
@@ -10,9 +10,72 @@
 //   console.log("keep coding")
 // }
 
-var hungry = true
-if (hungry !== true){
-  console.log("eat food")
-} else {
-  console.log("keep coding")
-}
+// var hungry = true
+// if (hungry !== true){
+//   console.log("eat food")
+// } else {
+//   console.log("keep coding")
+// }
+
+// var trafficLight = "green"
+// if (trafficLight === "green"){
+//   console.log("Go")
+// } else if (trafficLight !== "yellow") {
+//   console.log("Slow Down")
+// } else {
+//   console.log("Stop")
+// }
+
+// var trafficLight = "yellow"
+// if (trafficLight === "green"){
+//   console.log("Go")
+// } else if (trafficLight === "yellow") {
+//   console.log("Slow Down")
+// } else {
+//   console.log("Stop")
+// }
+
+// var trafficLight = "red"
+// if (trafficLight == "green"){
+//   console.log("Go")
+// } else if (trafficLight === "yellow") {
+//   console.log("Slow Down")
+// } else {
+//   console.log("Stop")
+// }
+
+// var smallNum = 0
+// var bigNum = 1
+// if (bigNum > smallNum) {
+//   console.log(bigNum)
+// }
+// else if (bigNum === smallNum) {
+//   console.log("The numbers are the same")
+// }
+// else {
+//   console.log("Try again")
+// }
+
+// var smallNum = 0
+// var bigNum = 0
+// if (bigNum > smallNum) {
+//   console.log(bigNum)
+// }
+// else if (bigNum === smallNum) {
+//   console.log("The numbers are the same")
+// }
+// else {
+//   console.log("Try again")
+// }
+
+// var smallNum = 1
+// var bigNum = 0
+// if (bigNum > smallNum) {
+//   console.log(bigNum)
+// }
+// else if (bigNum === smallNum) {
+//   console.log("The numbers are the same")
+// }
+// else {
+//   console.log("Try again")
+// }
