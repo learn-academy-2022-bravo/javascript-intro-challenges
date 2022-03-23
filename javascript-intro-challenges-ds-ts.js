58864a01c43080bf3e771b06f936457436c91a02
GeeeShep <gashep@yahoo.com>
2022-03-23 13:20:39 -0700

    input7 ts

--- javascript-intro-challenges-ds-ts.js
+++ javascript-intro-challenges-ds-ts.js
@@ -18,6 +18,7 @@ console.log(33 % 6)
 // Write the code that will log the length of a string containing your name.
 console.log("tony".length)
 // Write the code that will log whether your string includes the letter "e" ?
+console.log("tony".includes("e"))
 //     Write the code that will log the character at the first index of the string.
 // Write the code that will log the string in all uppercase letters.
 // Write the code that will log true or false for the following:
