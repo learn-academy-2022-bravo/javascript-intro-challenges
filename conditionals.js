3637d7ffd6e008bd05e6c4ec3848fe743fc7d484
cjpriedel <cjpriedel@gmail.com>
2022-03-23 15:59:28 -0700

    a few answers - cj

--- conditionals.js
+++ conditionals.js
@@ -1,1 +1,18 @@
-//JavaScript Conditional Challenges
+// //JavaScript Conditional Challenges
+
+// //Challenge #1
+
+// var item = 99
+
+// if (item <= 100) {
+//     console.log("In Budget")
+
+// //Challenge #2
+
+let hungry = true
+
+if (hungry == true) {
+    console.log("Eat Food!")
+} else {
+    console.log("Keep Coding")
+}
