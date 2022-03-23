8231f11fd2616cdfa146c221c882d4212b31b889
RiveraDarwin <dgrs1995@yahoo.com>
2022-03-23 13:06:34 -0700

    initial commit

---
+++ javascript-intro-challenges.js
@@ -0,0 +1,22 @@
+console.log(34 + 71); //1
+
+console.log(123-67); //2
+
+console.log(56*23); //3
+
+console.log(5**7); //4
+
+console.log(33%6); //5
+var myName = "Darwin"
+
+console.log("Darwin".length); //6
+
+console.log("Darwin".includes("e")); //7
+
+console.log("Darwin"[0]); //8
+console.log(myName.indexOf[1]); //8 //ask question about this
+
+console.log(); //
+console.log(); //
+console.log(); //
+console.log(); //
