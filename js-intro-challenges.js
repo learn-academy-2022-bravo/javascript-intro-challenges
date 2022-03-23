51262a5e8fe4ac455844650c8ce5cb3b1fc30200
cjpriedel <cjpriedel@gmail.com>
2022-03-23 14:03:18 -0700

    Completed Challenges, needs to be tested.

--- js-intro-challenges.js
+++ js-intro-challenges.js
@@ -1,2 +1,127 @@
 // JavaScript Intro Challenges
 // Challenge #1
+
+console.log(34 + 71)
+
+// Challenge #2
+
+console.log(67 - 123)
+
+// Challenge #3
+
+console.log(56 * 23)
+
+//Challenge #4
+
+console.log(45 / 5)
+
+// Challenge #5
+
+console.log(5 ** 7)
+
+// Challenge #6
+
+console.log(33 % 6)
+
+// Challenge #7
+
+var myName = "Cj"
+console.log(myName.length)
+
+// Challenge #8
+
+console.log(myName.includes(e))
+
+// Challenge #9
+
+console.log(myName.indexOf("0"))
+
+// Challenge #10
+
+console.log(myName.toUpperCase)
+
+// Challenge #11
+
+console.log(34 / 3 >= 67 / 2)
+
+// Challenge #12
+
+console.log(5 == "5")
+
+// Challenge #13
+
+console.log(5 === "5")
+
+// Challenge #14
+
+console.log(!3 === 3)
+
+// Challenge #15
+
+console.log("LEARN".length === 5 && "Student".length === 7)
+
+// Challenge #16
+
+console.log("LEARN".length === 5 || "Student".length === 10)
+
+// Challenge #17
+
+console.log("LEARN".includes("RN"))
+
+// Challenge #18
+
+console.log("LEARN".includes("rn"))
+
+//Challenge #19
+
+console.log("LEARN"[0] === "l"
+
+//Challenge #20
+
+console.log("LEARN"[0] === "L")
+
+//Challenge #21
+
+var theQuestion = "life, the universe, and everything"
+
+var theAnswer = 42
+
+console.log(theAnswer / 2)
+
+//Challenge #22
+
+console.log(theQuestion.length)
+
+//Challenge #23
+
+console.log(theQuestion.indexOf(f))
+
+//Challenge #24
+
+console.log(theQuestion + theAnswer)
+
+//Challenge #25
+
+console.log(theQuestion.charAt(7,8,9,10,11,12,13,14,15,16,17,18)
+***or*** 
+console.log(theQuestion.slice(7-18))
+
+//Challenge #26
+
+console.log(theQuestion.charAt(0))
+
+//Challenge #27
+
+
+console.log(theQuestion.length >= theAnswer)
+
+
+
+
+
+
+
+
+
+
+
