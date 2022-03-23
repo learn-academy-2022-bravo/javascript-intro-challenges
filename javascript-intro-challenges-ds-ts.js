38deabad6648b1212a49caf1316a4e49f6b621a8
GeeeShep <gashep@yahoo.com>
2022-03-23 13:37:50 -0700

    input13 ts

--- javascript-intro-challenges-ds-ts.js
+++ javascript-intro-challenges-ds-ts.js
@@ -32,6 +32,7 @@ console.log(5 == "5")
 //         Does 5 strictly equal "5" ?
 console.log(5 === "5")
 //             Does!3 strictly equal 3 ?
+console.log(!3 === 3)
 //                 Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7 ?
 //                     Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10 ?
 //                         Does "LEARN" contain the subset "RN" ?
