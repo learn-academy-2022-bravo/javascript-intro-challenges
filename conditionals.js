717a2cfab8a4ff26d3b45dcbcb34ea99ed4a4e3b
cjpriedel <cjpriedel@gmail.com>
2022-03-23 16:27:05 -0700

    Completed Conditionals challenges up to Stretch challenges

--- conditionals.js
+++ conditionals.js
@@ -1,13 +1,13 @@
 // //JavaScript Conditional Challenges
 
-// //Challenge #1
+//Challenge #1
 
-// var item = 99
+var item = 99
 
-// if (item <= 100) {
-//     console.log("In Budget")
+if (item <= 100) {
+    console.log("In Budget")
 
-// //Challenge #2
+//Challenge #2
 
 let hungry = true
 
@@ -16,3 +16,35 @@ if (hungry == true) {
 } else {
     console.log("Keep Coding")
 }
+
+//Challenge #3 (Not Working Properly)
+
+var trafficLight = "green"
+
+if (trafficLight === "green") {
+    console.log("Go!")
+} else if (trafficLight === "yellow") {
+    console.log("Slow Down!")
+} else (trafficLight === "red") ;
+    console.log("Stop!")
+
+//Challenge #4
+
+var largeNumber = 1000
+var smallNumber = 1 
+
+if (largeNumber >= smallNumber) {
+    console.log(largeNumber)
+} else (largeNumber === smallNumber) ;
+    console.log("The numbers are the same")
+}
+//Challenge #5
+
+var randomNumber = Math.random()
+if (randomNumber % 2 == 0) {
+    console.log("The number is even")
+} else {
+    console.log("The number is odd")
+}
+
+//Challenge #6
