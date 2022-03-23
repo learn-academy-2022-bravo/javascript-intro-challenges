99d4dd4458f715aba1fe16dce7af76fdb49a5e28
Dustin <42046575+dustin-oz@users.noreply.github.com>
2022-03-23 17:38:02 -0500

    Created conditionals file, pasted in questions and completed question 1 - ds

---
+++ conditionals-ds-ts.js
@@ -0,0 +1,22 @@
+// Consider the variables:
+
+var theQuestion = "life, the universe, and everything"
+
+var theAnswer = 42
+
+
+
+// Write the code that will log theAnswer divided by 2.
+console.log(theAnswer / 2)
+
+// Write the code that will log the length of theQuestion.
+
+// Write the code that will log the index of the character "f" in the theQuestion.
+
+// Write the code that will log the concatenation of the two variables.
+
+// Write the code that will log "the universe"
+
+// Write the code that will log the character "l" from theQuestion.
+
+// Write the code that will log whether theQuestion.length is greater than theAnswer.
