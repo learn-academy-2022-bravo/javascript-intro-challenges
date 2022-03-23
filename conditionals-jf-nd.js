67fc1d21d499c8328d4caed7f1dc42eb515361e6
nataliadiaz2 <natdiaz2001@gmail.com>
2022-03-23 15:53:23 -0700

    Start of our conditionals

---
+++ conditionals-jf-nd.js
@@ -0,0 +1,18 @@
+// var item = 100
+// if (item => 100){
+//   console.log("in budget")
+// }
+
+// var hungry = true
+// if (hungry === true){
+//   console.log("eat food")
+// } else {
+//   console.log("keep coding")
+// }
+
+var hungry = true
+if (hungry !== true){
+  console.log("eat food")
+} else {
+  console.log("keep coding")
+}
---
+++ javascript-intro-jf-nd.js
@@ -0,0 +1,57 @@
+// console.log(34 + 71)
+// 105
+// console.log(67 - 123)
+// -56
+// console.log(56 * 23)
+// 1288
+// console.log(45 / 5)
+// 9
+// console.log(5 ** 7)
+// 78125
+// console.log(33 % 6)
+// 3
+// console.log("Natalia".length)
+// 7
+// var greeting = "Hello"
+// console.log(greeting.includes("e"))
+// true
+// console.log(greeting.charAt(1))
+// e
+// console.log(greeting.toUpperCase())
+// HELLO
+// console.log(34 / 3 > 67 / 2)
+// false
+// console.log(5 == "5")
+// true
+// console.log(5 === "5")
+// false
+// console.log(!3 === 3)
+// false
+// console.log("LEARN".length === 5 && "Student".length === 7)
+// true
+// console.log("LEARN".length === 5 || "Student".length === 10)
+// true
+// console.log("LEARN".includes("RN"))
+// // true
+// console.log("LEARN".includes("rn"))
+// // false
+// console.log("LEARN"[0] === "l")
+// // false
+// console.log("LEARN"[0] === "L")
+// true
+var theQuestion = "life, the universe, and everything"
+var theAnswer = 42
+// console.log(theAnswer / 2)
+// 21
+// console.log(theQuestion.length)
+// 34
+// console.log(theQuestion.indexOf("f"))
+// 2
+// console.log(theAnswer + theQuestion)
+// 42life, the universe, and everything
+// console.log("the universe")
+// the universe
+// console.log(theQuestion.charAt(0))
+// l
+// console.log(theQuestion.length > theAnswer.length)
+// false
