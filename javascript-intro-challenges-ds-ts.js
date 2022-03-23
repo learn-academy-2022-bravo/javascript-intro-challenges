9963f7da734cc149b23a13eb0377d5417fd911a4
GeeeShep <gashep@yahoo.com>
2022-03-23 13:04:30 -0700

    input3 ts

--- javascript-intro-challenges-ds-ts.js
+++ javascript-intro-challenges-ds-ts.js
@@ -10,6 +10,7 @@ console.log(67 - 123)
 // Write the code that will log the outcome of 56 multiplied by 23.
 console.log(56 * 23)
 // Write the code that will log the outcome of 45 divided by 5.
+console.log(45 / 5)
 // Write the code that will log the outcome of 5 to the power of 7.
 // Write the code that will log the whole number remainder of 33 divided by 6.
 // Write the code that will log the length of a string containing your name.
