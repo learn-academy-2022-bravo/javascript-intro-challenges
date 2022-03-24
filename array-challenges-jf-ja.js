e8404f211e239f3ec068b3095e2dabb51baa592b
joagui83 <jjaggie83@gmail.com>
2022-03-24 13:28:21 -0700

    second completed array challenges

--- array-challenges-jf-ja.js
+++ array-challenges-jf-ja.js
@@ -21,8 +21,11 @@ var numbers = [2, 4, 6, 8, 10]
 // console.log(numbers.unshift(0))
 // console.log(numbers)
 // Write the code that will add the number 12 to the end of the array.
-console.log(numbers.push(12))
-console.log(numbers)
+// console.log(numbers.push(12))
+// console.log(numbers)
 // Write the code that will remove the first number from the array.
-
+// console.log(numbers.shift())
+// console.log(numbers)
 // Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
+let num = [0]
+console.log(num.concat(numbers))
