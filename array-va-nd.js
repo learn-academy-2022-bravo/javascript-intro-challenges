f1ffbe1e70bd6ccde44badb7d01e168679c77767
aVictorWXYZ <victorargote@gmail.com>
2022-03-24 12:08:35 -0700

    second commit

--- array-va-nd.js
+++ array-va-nd.js
@@ -5,5 +5,35 @@ var groceryList = ["chips", "dip", "cookies"]
 // [ 'chips', 'dip', 'cookies', 'soda', 'granola' ]
 // console.log(groceryList.slice(0,2))
 // [ 'chips', 'dip' ]
-console.log(groceryList[0].pop())
-console.log(groceryList)
+// console.log(groceryList.pop())
+// console.log(groceryList.reverse())
+
+// console.log(groceryList.push("beans"))
+
+// console.log(groceryList.reverse())
+// var numbers = [2, 4, 6, 8, 10]
+// var zero = [0]
+
+// console.log(numbers.reverse())
+// console.log(numbers.push(0))
+// console.log(numbers.reverse())
+
+// console.log(zero.concat(numbers))
+// console.log(numbers.push(12))
+
+// console.log(numbers.slice(1))
+
+// console.log(zero.concat(numbers))
+
+// console.log(numbers)
+
+var numSet = [2, 13, 6, 8, 4, 2]
+
+
+// console.log(numSet.indexOf(2))
+
+// console.log(numSet.lastIndexOf(2))
+
+
+
+console.log(numSet.slice(3,4))
