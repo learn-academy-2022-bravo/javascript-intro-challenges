aa1ec50151c3e95baf048bdc2c893fa34e661c31
khourysamir <samir.khoury1998@gmail.com>
2022-03-24 14:18:53 -0700

    ARRAYS

---
+++ arrays-sk-dr.js
@@ -0,0 +1,90 @@
+//Number 1
+
+//let groceryList = ["chips", "dip", "cookies"]
+
+//console.log(groceryList.push("soda"))
+
+//Number 2
+
+//console.log(groceryList.pop("soda"))
+
+//console.log(groceryList.push("granola"))
+
+//console.log(groceryList)
+
+//Number 3
+
+//console.log(groceryList.slice(0,2))
+
+//Number 4
+
+//console.log(groceryList.slice(0,2).concat("beans"))
+
+//Number 5
+
+//let numbers = [2, 4, 6, 8, 10]
+
+//console.log(numbers.push(0))
+//console.log(numbers.sort())
+
+//Number6
+
+// console.log(numbers.push(12))
+// console.log(numbers)
+
+// //Number 7
+
+// console.log(numbers.reverse())
+// console.log(numbers.pop())
+// console.log(numbers.reverse())
+
+//Number 8
+
+// let numbers = [2, 4, 6, 8, 10]
+
+// console.log(numbers.splice(0,0,0))
+// console.log(numbers)
+
+//Number 9
+
+ //let numSet = [2, 13, 6, 8, 4, 2]
+
+// console.log(numSet.indexOf(2))
+
+//Number 10
+
+//console.log(numSet.indexOf(2,2))
+
+//Number 11
+
+//console.log(numSet[2])
+
+//Number 12
+
+//let characters = ["y", "a", "r", "r", "a"]
+
+//console.log(characters.join(""))
+
+//let charsReversed = characters.reverse()
+//console.log(charsReversed)
+
+//Number 14
+
+//console.log(charsReversed.join("*"))
+
+//Number 15
+
+//console.log(charsReversed.join(" "))
+
+//Number 16-18
+
+//let classOne = ["Jack", "Toni", "Neco"]
+//let classTwo = ["Samir","Darwin","CJ"]
+
+//console.log(classOne.concat(classTwo).sort());
+
+//Number 19
+
+let numbers = [42, 221, 71, 7, 18, 87]
+let oddIndexes = []
+console.log(numbers)
