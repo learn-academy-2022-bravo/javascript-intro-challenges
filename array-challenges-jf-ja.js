29a4ea99d5c7227985776452120613cc376ea495
JonathnSparePartsFuller <jonefuller@gmail.com>
2022-03-24 13:56:49 -0700

    12 down

--- array-challenges-jf-ja.js
+++ array-challenges-jf-ja.js
@@ -15,17 +15,49 @@
 //
 // console.log(groceryList.slice(0, 2))
 // ['chips', 'dip' ]
+//
+// var numbers = [2, 4, 6, 8, 10]
+// // Write the code that will add the number 0 to the beginning of the array.
+// // console.log(numbers.unshift(0))
+// // console.log(numbers)
+// // Write the code that will add the number 12 to the end of the array.
+// // console.log(numbers.push(12))
+// // console.log(numbers)
+// // Write the code that will remove the first number from the array.
+// // console.log(numbers.shift())
+// // console.log(numbers)
+// // Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
+// let num = [0]
+// console.log(num.concat(numbers))
 
-var numbers = [2, 4, 6, 8, 10]
-// Write the code that will add the number 0 to the beginning of the array.
-// console.log(numbers.unshift(0))
-// console.log(numbers)
-// Write the code that will add the number 12 to the end of the array.
-// console.log(numbers.push(12))
-// console.log(numbers)
-// Write the code that will remove the first number from the array.
-// console.log(numbers.shift())
-// console.log(numbers)
-// Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
-let num = [0]
-console.log(num.concat(numbers))
+// var numSet = [2, 13, 6, 8, 4, 2]
+// Write the code that finds the index of the first appearance of the number 2.
+// console.log(numSet.indexOf(2))
+// 0
+// Write the code that finds the index of the last appearance of the number 2.
+// console.log(numSet.lastIndexOf(2))
+//5
+// Write the code that returns the number at the third index.
+// console.log(numSet.slice(3, 4))
+// [8]
+// Consider the variable:
+
+var characters = ["y", "a", "r", "r", "a"]
+// Write the code that brings all the letters in the characters array together into a string.
+// console.log (characters.join(""))
+// yarra
+// Write the code that reverses the order of the letters in the characters array and saves it into a variable called charsReversed.
+//
+// Write the code that brings all the letters in the charsReversed array together into a string with an asterisk between each letter.
+//
+// Write the code that brings all the letters in the charsReversed array together into a string without separators.
+//
+// Create two arrays consisting of three first names of your cohort members in each.
+//
+// Write the code that sorts the names in alphabetical order.
+//
+// Write the code that sorts the names in reverse alphabetical order.
+//
+// Write the code that sorts all the names in alphabetical order in a single array.
+//
+// Consider the variables:
