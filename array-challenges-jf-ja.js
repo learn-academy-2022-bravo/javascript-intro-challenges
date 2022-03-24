da7f7fbd583a70a88ff6e43648aab289533d2045
JonathnSparePartsFuller <jonefuller@gmail.com>
2022-03-24 11:50:19 -0700

    Quarter way there

---
+++ array-challenges-jf-ja.js
@@ -0,0 +1,20 @@
+var groceryList = ["chips", "dip", "cookies"]
+// Write the code that will add "soda" to the end of the original array.
+//
+// Write the code that will add "granola" to the end of array without altering the original array.
+//
+// Write the code that will return a subset of the array containing only "chips" and "dip".
+//
+// Write the code that will add "beans" to the "chips" and "dip" array.
+
+// console.log(groceryList.push("soda"))
+// 4
+// let groceryList2 = ["granola"]
+// console.log(groceryList.concat(groceryList2))
+// [ 'chips', 'dip', 'cookies', 'granola' ]
+//
+// console.log(groceryList.slice(0, 2))
+// ['chips', 'dip' ]
+console.log(groceryList.pop())
+let groceryList2 = ["beans"]
+console.log(groceryList.concat(groceryList2))
