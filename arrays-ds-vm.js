de6db95a3f050b3cc13348bcdfb2a2d5b6069360
Dustin <42046575+dustin-oz@users.noreply.github.com>
2022-03-24 14:07:06 -0500

    Completed a few array questions

--- arrays-ds-vm.js
+++ arrays-ds-vm.js
@@ -22,18 +22,29 @@
 
 // Consider the variable:
 
-// var numbers = [2, 4, 6, 8, 10]
 // Write the code that will add the number 0 to the beginning of the array.
+// console.log(numbers.unshift(0))
+// console.log(numbers)
 
 // Write the code that will add the number 12 to the end of the array.
-
+// console.log(numbers)
+// console.log(numbers.push(12))
+// console.log(numbers)
 // Write the code that will remove the first number from the array.
+// console.log(numbers)
+// console.log(numbers.shift())
+// console.log(numbers)
 
 // Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
+// var numbers = [2, 4, 6, 8, 10]
+// var numberZero = [0]
+// console.log(numbers)
+// console.log(numbers.concat(numberZero))
+// console.log(numbers)
 
 // Consider the variable:
 
-// var numSet = [2, 13, 6, 8, 4, 2]
+var numSet = [2, 13, 6, 8, 4, 2]
 // Write the code that finds the index of the first appearance of the number 2.
 
 // Write the code that finds the index of the last appearance of the number 2.
