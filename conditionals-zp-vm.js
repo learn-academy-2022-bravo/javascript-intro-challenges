7efeb7ad2b4e7373739a8640c59e2c156bfe53cd
VMMARIN <vanessam.marin@gmail.com>
2022-03-24 11:50:25 -0700

    first push

---
+++ conditionals-zp-vm.js
@@ -0,0 +1,67 @@
+// Write a statement that takes a variable of item and logs "in budget" if a price is $100 or less.
+
+// var myNum = 50
+// if(myNum<=100)
+//  {
+//      console.log("in budget")
+// }
+
+// Write a statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
+
+//  var imHungry = true
+
+// if(imHungry = true) {
+//   console.log("eat food.")
+// } else {
+//   console.log("keep coding.")
+// }
+
+// var imHungry = true 
+
+// if(imHungry = false) {
+//   console.log("eat food.")
+// } else {
+//   console.log("keep coding.")
+// }
+
+// Write a statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
+
+
+
+// if(trafficLight = green) {
+//   console.log("go.")
+// } else if(trafficLight = yellow) {
+//   console.log("slow down.")
+// } else {
+//   console.log("stop.")
+// }
+
+
+// var trafficLight = false
+
+// if(trafficLight === true) {
+//   console.log("go.")
+// } else if(trafficLight === false) {
+//   console.log("slow down.")
+// } else {
+//   console.log("stop.")
+// }
+
+
+// Write a statement that takes two variables that are numbers and outputs the larger number. If the numbers are equal, output "the numbers are the same".
+ 
+// console.log(5>4) {
+//     if true console.log("5")
+// } else if
+//     console.log("the numbers are the same")
+// }
+
+// Write a statement that takes a variable of a number and logs whether the number is odd, even, or zero.
+
+// STRETCH Challenges
+// Write a statement that takes a variable of a grade percentage and logs the letter grade for that percentage, if the grade is 100% log "perfect score", if the grade is zero log "no grade available."
+
+// Write a statement that takes a variable of a boolean, number, or string data type and logs the data type of the variable. HINT: Check out the JavaScript typeof operator.
+
+// Create a password checker using a single conditional statement. If a user inputs a password with 12 or more characters AND the password includes !, then log "That is a mighty strong password!" If the user’s password is 8 or more characters OR includes !, then log "That password is strong enough." Log "That is not a valid password." for every other input.
+
