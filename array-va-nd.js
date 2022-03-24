41f907245d650e0f67f2d0ec3561945b13768390
nataliadiaz2 <natdiaz2001@gmail.com>
2022-03-24 11:46:22 -0700

    Array Challenge

---
+++ array-va-nd.js
@@ -0,0 +1,9 @@
+var groceryList = ["chips", "dip", "cookies"]
+// console.log(groceryList.push("soda"))
+// [ 'chips', 'dip', 'cookies', 'soda' ]
+// console.log(groceryList.concat("granola"))
+// [ 'chips', 'dip', 'cookies', 'soda', 'granola' ]
+// console.log(groceryList.slice(0,2))
+// [ 'chips', 'dip' ]
+console.log(groceryList[0].pop())
+console.log(groceryList)
