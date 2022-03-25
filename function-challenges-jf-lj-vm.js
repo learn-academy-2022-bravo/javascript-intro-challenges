9df44b1b0d58516696a77ee1da9cae17b6c4618c
ljkotalik <leannak14@gmail.com>
2022-03-25 12:59:59 -0700

    7 out of 10 questions

--- function-challenges-jf-lj-vm.js
+++ function-challenges-jf-lj-vm.js
@@ -14,31 +14,48 @@
 
 
 // Write a function named oddOrEven that takes a number as an argument and returns whether the number is odd or even.
-const oddOrEven = (num) => {
-  if (num === "1"){
-    return `${num} is odd`
-  }else (num === "2");{
-    return `${num} is even`
-  }
-}
-console.log(oddOrEven("1"))
-console.log(oddOrEven("2"))
+// const oddOrEven = (num) => {
+//   if (num === "1"){
+//     return `${num} is odd`
+//   }else (num === "2");{
+//     return `${num} is even`
+//   }
+// }
+// console.log(oddOrEven("1"))
+// console.log(oddOrEven("2"))
 
 
 
 // Write a function named triple that takes a number as an argument and returns the result of that number multiplied by 3.
-//
 
+//  const triple = (num1) => {
+//    return num1 * 3
+//  }
+//  console.log(triple(2))
 
 
 // Write a function named multiply that takes two numbers as arguments and returns the result of the numbers multiplied together.
-//
+//  const triple = (num1, num2) => {
+//    return num1 * num2
+//  }
+//  console.log(triple(6, 4))
 
 
 // Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
 //
+// const divisibleBy = (num1, num2) => {
+//   return `${num1} is evenly divisible by ${num2}` 
+// }
+
+// console.log(divisibleBy(10, 5))
+
 // Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
-//
+const assignGrade = () => {
+  return "A"
+}
+console.log(assignGrade(100))
+
+
 // Write a function named isLonger that takes two strings as arguments and returns the string that contains the most characters.
 //
 // Write a function named greaterNum that takes two numbers as arguments and returns whichever number is the greater (higher) number.
