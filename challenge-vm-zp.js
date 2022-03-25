225788428d0488a301564bc54f1ccc9a141eda23
LJ Kotalik <101531279+ljkotalik@users.noreply.github.com>
2022-03-25 08:29:31 -0700

    Revert "Arrays al lj"

---
+++ challenge-vm-zp.js
@@ -0,0 +1,43 @@
+// console.log(34 + 71)
+
+//console.log (123 - 67)
+
+//console.log(56 * 23)
+
+// console.log(45 / 5);
+
+// console.log(5 ** 7);
+
+// console.log(33 % 6);
+
+// console.log("zane and vanessa".length);
+
+// console.log("zane and vanessa".length);
+
+// var greeting = "zane and vanessa"
+
+// console.log (greeting.includes("e"));
+
+// console.log(greeting.charAt(0));
+
+// console.log(greeting.toUpperCase());
+
+//console.log("34/3">"67/2")
+
+//console.log (5 == "5")
+
+//console.log(!3===3)
+
+//console.log("learn".length===5 && "student".length===7)
+
+//console.log("learn".length===5 && "student".length===10)
+
+//var greeting = "learn"
+//console.log(greeting.includes("rn"))
+
+//var greeting = "learn"
+//console.log(greeting.includes("RN"))
+
+
+//var greeting = "learn"
+//console.log(greeting.charAt(0)==="l")
