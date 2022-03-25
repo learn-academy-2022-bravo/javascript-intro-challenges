2f06a7a75a333c38e6adcc392fa56f8d00a2a665
ecostrong <necoarmstrong@mac.com>
2022-03-25 11:42:37 -0700

    completed problem two

--- func-loops-arrays-ds-na.js
+++ func-loops-arrays-ds-na.js
@@ -6,22 +6,41 @@
 // var testArr1 = [3, 9, 15, 4, 10]
 // --> [9, 27, 45, 12, 30]
 
-var theArray = [3, 9, 15, 4, 10]
+//var theArray = [3, 9, 15, 4, 10]
 
-const numbersMultipled = (theArray) => {
-    let newArray = []
-    for(let i=0; i<theArray.length; i++){
-      newArray.push(theArray[i]*3)
-    }
-    return newArray
-}
-console.log(numbersMultipled(theArray))
-
-
+// const numbersMultipled = (theArray) => {
+//     let newArray = []
+//     for(let i=0; i<theArray.length; i++){
+//       newArray.push(theArray[i]*3)
+//     }
+//     return newArray
+// }
+// console.log(numbersMultipled(theArray))
 
 // Write a function that takes in an array of numbers and returns a new array with only odd numbers.
 // var testArr2 = [0, 2, -7, 3, 5, 8, 10, 13]
-// // --> [-7, 3, 5, 13]
+// --> [-7, 3, 5, 13]
+
+var theArray = [0, 2, -7, 3, 5, 8, 10, 13]
+
+const onlyOdd = (theArray) => {
+  let newArray = []
+  for (let i=0; i<theArray.length; i++){
+    if (theArray[i] % 2 !== 0){
+      newArray.push(theArray[i])
+    }
+  }
+   return newArray
+}
+console.log(onlyOdd(theArray))
+
+
+
+
+
+
+
+
 // Write a function that takes in an array of numbers and letters and returns a string with only the letters. HINT: use the typeof method.
 // var comboArr = [7, "n", true, "i", "c", 10, "e", -388, "w", 3, "o", 0, "r", false, "k"]
 // // --> "nicework"
