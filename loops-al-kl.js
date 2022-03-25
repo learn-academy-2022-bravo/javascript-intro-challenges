2f57b3104ac1361a3846baf5637e116b00cfe551
lundrew <andrueee@gmail.com>
2022-03-25 09:21:03 -0700

    branch update

--- loops-al-kl.js
+++ loops-al-kl.js
@@ -18,22 +18,29 @@
 
 
 // Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD".
-for (var i = 1; i < 21; i++) {
-  if([i] % 2 == 0){
-    console.log(i);
-  } else if ([i] % 2 !== 0){
-    console.log("ODD");}
+// Expected output: ODD, 2, ODD, 4, ODD, 6 ...etc
+// for (var i = 1; i < 21; i++) {
+//   if([i] % 2 == 0){
+//     console.log(i);
+//   } else if ([i] % 2 !== 0){
+//     console.log("ODD");}
+// }
+
+
+// Consider this variable:
+const nums = [3, 57, -9, 20, 67]
+// Create the code that will log the largest number from the array.
+
+for (let i = 0; i < nums.length; i++) {
+  array[i]
 }
 
 
 
 
 
-// Expected output: ODD, 2, ODD, 4, ODD, 6 ...etc
-// Consider this variable:
-//
-// const nums = [3, 57, -9, 20, 67]
-// Create the code that will log the largest number from the array.
+
+
 // Create the code that will log the smallest number from the array.
 // Create the code that will log the remainder of each number when divided by 2.
 // Expected output: 1, 1, -1, 0, 1
